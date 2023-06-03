eeccdaaf35675061414bacea90c6e9f283519340
JRosSx91 <hazee.1491@gmail.com>
2023-06-03 19:23:39 +0200

    create star and particle var

--- gravity-game/src/main.rs
+++ gravity-game/src/main.rs
@@ -12,4 +12,14 @@ fn main() {
         .exit_on_esc(true)
         .build()
         .unwrap();
+
+    let mut star = Particle {
+        x: 320.0,
+        y: 240.0,
+        vx: 0.0,
+        vy: 0.0,
+        size: 10.0,
+    };
+
+    let mut particles = vec![];
 }
