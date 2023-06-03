ebb950ac13480cca14fb9b088ad452d2cf3c226e
JRosSx91 <hazee.1491@gmail.com>
2023-06-03 19:20:25 +0200

    create project

---
+++ gravity-game/target/debug/deps/gravity_game-33d4533fb2b295b7.d
@@ -0,0 +1,5 @@
+C:\Users\Ivan\labs\rust\Gravity-Game\gravity-game\target\debug\deps\gravity_game-33d4533fb2b295b7.rmeta: src\main.rs
+
+C:\Users\Ivan\labs\rust\Gravity-Game\gravity-game\target\debug\deps\gravity_game-33d4533fb2b295b7.d: src\main.rs
+
+src\main.rs:
